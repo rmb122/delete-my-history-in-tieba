8d9e1b5efbe150c70854be62b0d2834778f96f4b
rmb122 <33388702+rmb122@users.noreply.github.com>
2017-12-11 23:26:49 +0800

    Update TieBaDeleter.py

--- TieBaDeleter.py
+++ TieBaDeleter.py
@@ -45,7 +45,7 @@ def my_reply_collector():
     print("Links of Reply Collected")
     return listOfLinks
 
-def deleter(listOfLinks):
+def deleter_tie(listOfLinks):
     print("Now Deleting")
     for i in range(0,len(listOfLinks)-1):
         try:
@@ -59,6 +59,30 @@ def deleter(listOfLinks):
         except common.exceptions.NoSuchElementException:
             print("Fail to find the element") #古老版本匿名,或者隐藏帖子没有删除按钮
 
+def deleter_follows():
+    while True:
+        driver.get("http://tieba.baidu.com/i/i/concern")
+        try:
+            driver.find_element_by_class_name("btn_unfollow").click()
+            driver.find_element_by_class_name("dialogJbtn").click()
+            time.sleep(0.5)
+        except common.exceptions.NoSuchElementException:
+            print("Follows has been all deleted")
+            break
+            
+def deleter_fans():
+    while True:
+        driver.get("http://tieba.baidu.com/i/i/fans")
+        try:
+            element=driver.find_element_by_class_name("name")
+            ActionChains(driver).move_to_element(element).perform() #移动到名字否则取消关注不会出现
+            driver.find_element_by_id("add_blacklist_btn").click()
+            driver.find_element_by_class_name("dialogJbtn").click()
+            time.sleep(0.5)
+        except common.exceptions.NoSuchElementException:
+            print("Fans has been all deleted")
+            break   
+
 def Start_with_Chrome():
     chrome_options = webdriver.ChromeOptions()
     prefs = {"profile.managed_default_content_settings.images":2} #不加载图片
@@ -68,4 +92,7 @@ def Start_with_Chrome():
 
 driver=Start_with_Chrome()
 login("Here is your username","Here is your password")
-deleter(my_reply_collector()) #Or my_tie_collector()
+deleter_tie(my_reply_collector()) #Or my_tie_collector()
+deleter_fans()
+deleter_follows()
+print("All done")
