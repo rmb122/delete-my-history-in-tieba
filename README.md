0e71b1ddda22ea91d89797944dd156b7213f71d9
rmb122 <me@rmb122.cn>
2018-04-30 23:48:39 +0800

    rebuild

--- README.md
+++ README.md
@@ -2,15 +2,14 @@
 
 ## 使用之前
 
-运行在Python 3.6  
-Delete reply by urllib.py 换成自己的 cookie 后可以直接使用  
-TieBaDeleter.py 需要下载 selenium 的 webdriver 库  
-
 ```sh  
-pip install selenium  
+pip install -r requirements.txt
 ```
+注意还需要安装 ChromeDriver 才可以使用 TieBaDeleter.py  
 
-以及安装 ChromeDriver 后方可使用   
+运行在Python 3.6  
+TieBaDeleter.py 可以删除关注、粉丝、帖子、回复  
+Delete-reply.py 只能删除回复但是速度快  
 
 ## 安装ChromeDriver
 
@@ -20,25 +19,23 @@ pip install selenium
 
 ## TieBaDeleter.py
 
-login() 登陆 替换成自己的用户密码即可   
+login() 登陆 替换成自己的用户密码即可  
 my_tie_collector() 获得自己发的帖子的链接  
-my_reply_collector() 获得自己回复的链接  
-deleter_tie() 删除帖子 注意需配合my_tie_collector()和my_reply_collector()    
-使用而且坑爹的百度每天只能删30条  
+my_reply_collector() 获得自己回复的链接    
+deleter_tie() 删除帖子 注意需配合 my_tie_collector() 和 my_reply_collector()  
 deleter_follows() 删除关注  
 deleter_fan() 删除粉丝  
 deleter_BaIFollow() 删除自己关注的吧  
-根据自动的网络情况调整sleep()里面的时间  
+根据自动的网络情况调整 sleep() 里面的时间  
 
-## Delete reply by urllib.py
+## Delete-reply.py
 
-用 urllib 重构的删除回复版本, 删的比 selenium 快而且能在命令行运行  
-需要在相同目录下添加自己的 Cookie 文件, JSON 格式  
+用 requests 重构的删除回复版本, 删的比 selenium 快很多  
+需要在 cookie.json 中添加自己的 Cookie, JSON 格式  
 我用的是 Chrome 的 [EditThisCookie](2) 插件, 可以直接导出 JSON 下的 Cookie  
 
-帖子多的话可以放在 vps 上每天自动运行    
-Delete reply by urllib_server.py 即是这个的服务器版, 加入到计划任务中每日运行就可以啦  
-PS: 记得根据自己的情况调整在文件中的搜索回复贴子的起始和结束页码  
+帖子多的话可以放在 vps 上每天自动运行, 加入到计划任务中就可以啦  
+PS: 记得根据自己的情况调整在文件中的搜索回复贴子的起始和结束页码 (百度有各种奇葩的 bug)  
 
-[1]:https://sites.google.com/a/chromium.org/chromedriver/
-[2]:https://chrome.google.com/webstore/detail/editthiscookie/fngmhnnpilhplaeedifhccceomclgfbg
+[1]: https://sites.google.com/a/chromium.org/chromedriver/downloads
+[2]: https://chrome.google.com/webstore/detail/editthiscookie/fngmhnnpilhplaeedifhccceomclgfbg
--- TieBaDeleter.py
+++ TieBaDeleter.py
@@ -1,61 +1,71 @@
-from selenium import webdriver,common
-import time
 import re
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
 import urllib.request
-from selenium.webdriver.common.action_chains import ActionChains 
 
-def login(username,password):
+from selenium import common, webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+def login(username, password):
     print("Spider started")
-    urlusername=urllib.request.quote(username) 
-    driver.get("http://tieba.baidu.com/home/main?un="+urlusername+"&fr=home")
+    urlusername = urllib.request.quote(username)
+    driver.get("http://tieba.baidu.com/home/main?un=" + urlusername + "&fr=home")
     driver.find_element_by_class_name("u_login").click()
     time.sleep(0.5)
-    driver.find_element_by_id("TANGRAM__PSP_8__userName").send_keys(username)
-    driver.find_element_by_id("TANGRAM__PSP_8__password").send_keys(password)
-    driver.find_element_by_id("TANGRAM__PSP_8__submit").click()
+
+    pageSouce = driver.page_source
+    elementidPrefix = re.findall(r"(TANGRAM__PSP_[0-9]{1,})__footerULoginBtn", pageSouce)[0]
+    
+    driver.find_element_by_id(elementidPrefix + "__footerULoginBtn").click()
+    driver.find_element_by_id(elementidPrefix + "__userName").send_keys(username)
+    driver.find_element_by_id(elementidPrefix + "__password").send_keys(password)
+    driver.find_element_by_id(elementidPrefix + "__submit").click()
     time.sleep(3)
 
-def my_tie_collector():  
+
+def my_tie_collector():
     driver.get("http://tieba.baidu.com/i/i/my_tie")
-    listOfLinks=list()
-    listOfElements=driver.find_elements_by_class_name("thread_title")
-    for i in range(0,len(listOfElements)):
+    listOfLinks = list()
+    listOfElements = driver.find_elements_by_class_name("thread_title")
+    for i in range(0, len(listOfElements)):
         listOfLinks.append(listOfElements[i].get_attribute("href"))
 
-    driver.get("http://tieba.baidu.com/i/i/my_tie?&pn=2")  #注意每天限制30贴，所以最多前两页就足够了
-    listOfElements=driver.find_elements_by_class_name("thread_title")
-    for i in range(0,len(listOfElements)):
+    driver.get("http://tieba.baidu.com/i/i/my_tie?&pn=2")  #每天限制30贴，所以最多前两页就足够了
+    listOfElements = driver.find_elements_by_class_name("thread_title")
+    for i in range(0, len(listOfElements)):
         listOfLinks.append(listOfElements[i].get_attribute("href"))
 
     print("Links of Tie Collected")
     return listOfLinks
 
+
 def my_reply_collector():
     driver.get("http://tieba.baidu.com/i/i/my_reply")
-    listOfLinks=list()
-    listOfElements=driver.find_elements_by_class_name("for_reply_context")
-    for i in range(0,len(listOfElements)):
+    listOfLinks = list()
+    listOfElements = driver.find_elements_by_class_name("for_reply_context")
+    for i in range(0, len(listOfElements)):
         listOfLinks.append(listOfElements[i].get_attribute("href"))
 
-    driver.get("http://tieba.baidu.com/i/i/my_reply?&pn=2")  #同上
-    listOfElements=driver.find_elements_by_class_name("for_reply_context")
-    for i in range(0,len(listOfElements)):
+    driver.get("http://tieba.baidu.com/i/i/my_reply?pn=2")  #同上
+    listOfElements = driver.find_elements_by_class_name("for_reply_context")
+    for i in range(0, len(listOfElements)):
         listOfLinks.append(listOfElements[i].get_attribute("href"))
 
     print("Links of Reply Collected")
     return listOfLinks
 
-def deleter_tie(listOfLinks,username): #增加对楼中楼的删除功能
+
+def deleter_tie(listOfLinks, username):  #增加对楼中楼的删除功能
     print("Now Deleting")
-    for i in range(0,len(listOfLinks)):
+    for i in range(0, len(listOfLinks)):
         try:
             driver.get(listOfLinks[i])
-            time.sleep(1) 
-            element=driver.find_element_by_class_name("p_post_del_my")
+            time.sleep(1)
+            element = driver.find_element_by_class_name("p_post_del_my")
             driver.execute_script("arguments[0].scrollIntoView(false);", element)
             element.click()
-            time.sleep(0.3)                  
+            time.sleep(0.3)
             driver.find_element_by_class_name("dialogJanswers").find_element_by_tag_name("input").click()
             print("Deleted")
             continue
@@ -63,21 +73,23 @@ def deleter_tie(listOfLinks,username): #增加对楼中楼的删除功能
             print("Fail to find element,try next way")
 
         try:
-            maincontent=driver.find_element_by_class_name("p_postlist")
-            elements=maincontent.find_elements_by_link_text(username)
+            maincontent = driver.find_element_by_class_name("p_postlist")
+            elements = maincontent.find_elements_by_link_text(username)
             for element in elements:
                 ActionChains(driver).move_to_element(element).perform()
                 try:
                     driver.find_element_by_link_text("删除").click()
                     break
                 except common.exceptions.NoSuchElementException:
-                    print("Fail to find element,try next herf") #有可能别人@你导致选错元素，所以对每个超链接遍历一遍直到找到有删除按钮的
-            time.sleep(0.3)                  
-            driver.find_element_by_class_name("dialogJanswers").find_element_by_tag_name("input").click()
+                    print("Fail to find element,try next herf")  #有可能别人@你导致选错元素，所以对每个超链接遍历一遍直到找到有删除按钮的
+            time.sleep(0.3)
+            driver.find_element_by_class_name(
+                "dialogJanswers").find_element_by_tag_name("input").click()
             print("Deleted")
         except common.exceptions.NoSuchElementException:
             print("Fail to find element,maybe your are in anonymous")
-            
+
+
 def deleter_follows():
     while True:
         driver.get("http://tieba.baidu.com/i/i/concern")
@@ -88,21 +100,23 @@ def deleter_follows():
         except common.exceptions.NoSuchElementException:
             print("Follows has been all deleted")
             break
-            
+
+
 def deleter_fans():
     while True:
         driver.get("http://tieba.baidu.com/i/i/fans")
         try:
-            element=driver.find_element_by_class_name("name")
-            ActionChains(driver).move_to_element(element).perform() #移动到名字否则取消关注不会出现
+            element = driver.find_element_by_class_name("name")
+            ActionChains(driver).move_to_element(element).perform()  #移动到名字否则取消关注不会出现
             driver.find_element_by_id("add_blacklist_btn").click()
             driver.find_element_by_class_name("dialogJbtn").click()
             time.sleep(0.5)
         except common.exceptions.NoSuchElementException:
             print("Fans has been all deleted")
-            break   
-            
-def deleter_BaIFollow(): #使用此功能需打开图片显示
+            break
+
+
+def deleter_BaIFollow():  #使用此功能需打开图片显示
     driver.get("http://tieba.baidu.com/i/i/forum")
     driver.find_element_by_class_name("pm_i_know").click()
     while True:
@@ -113,17 +127,27 @@ def deleter_BaIFollow(): #使用此功能需打开图片显示
         except common.exceptions.NoSuchElementException:
             print("BaIFollows has been all deleted")
             break
-            
-def Start_with_Chrome():
+
+
+def Start_with_Chrome_without_images():
     chrome_options = webdriver.ChromeOptions()
-    prefs = {"profile.managed_default_content_settings.images":2} #不加载图片 若注释掉这行和下一行即加载图片
-    chrome_options.add_experimental_option("prefs",prefs)
-    driver=webdriver.Chrome(chrome_options=chrome_options)
+    prefs = {
+        "profile.managed_default_content_settings.images": 2
+    }
+    chrome_options.add_experimental_option("prefs", prefs)
+    driver = webdriver.Chrome(chrome_options=chrome_options)
     return driver
 
-driver=Start_with_Chrome()
-login("Here is your username","Here is your password")
-deleter_tie(my_reply_collector(),"Here is your username") #Or my_tie_collector()
+
+def Start_with_Chrome():
+    driver = webdriver.Chrome()
+    return driver
+
+
+driver = Start_with_Chrome()  #使用 Start_with_Chrome_without_images 速度更快但注意 deleter_BaIFollow 不能使用
+login("Here is your username", "Here is your password")
+deleter_tie(my_reply_collector(), "Here is your username")  #或者 my_tie_collector()
 deleter_fans()
 deleter_follows()
+deleter_BaIFollow()
 print("All done")
